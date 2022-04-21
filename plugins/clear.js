0f12c25c033bc8f114ce8addb842398c0828028b
Beyzamtek <103938119+Beyzamtek@users.noreply.github.com>
2022-04-21 17:04:44 +0300

    Update clear.js

--- plugins/clear.js
+++ plugins/clear.js
@@ -1,12 +1,4 @@
-let {MessageType, Presence, GroupSettingChange, ChatModification, WAConnectionTest} = require('@adiwajshing/baileys');
-let Bot = require('../events');
-let fs = require('fs');
-let Config = require('../config');
-let Language = require('../language');
-let Lang = Language.getString('clear');
 
-/*
-var data = { action: true }
 *⦁══════✅══════⦁*
  CHAT TEMIŹLENİYOR
 *⦁══════✅══════⦁*
